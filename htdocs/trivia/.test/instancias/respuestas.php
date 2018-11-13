8562d80f72183f3df1d1d771f4da9e682955dfef
kerberos <kerberos119@gmail.com>
2018-11-13 12:16:47 -0600

    Cambios menores

--- htdocs/trivia/.test/instancias/respuestas.php
+++ htdocs/trivia/.test/instancias/respuestas.php
@@ -7,13 +7,11 @@ $respuesta = $_POST['respuesta'];//cogemos la respuesta del usuario
 //SI LA RESPUESTA ES CORRECTA SUMAMOS 10 PUNTOS Y MOSTRAMOS OTRA DE NUEVO
 if($correcta == $respuesta)
 {
-	$_SESSION['respuesta'] = "<div id=\"error\"></div>";
 	$_SESSION['puntos'] += 10;
 	$trivial->update_puntuacion($_SESSION['puntos'],$_SESSION['nombre']);
 	header("Location: ../old.preguntas_trivial.php");
 //SI NO ES CORRECTA MOSTRAMOS QUE NO LO ES Y MOSTRAMOS OTRA NUEVA
 }else{
-	$_SESSION['respuesta'] = "<div id=\"success\"></div>";
 	$_SESSION['puntos'] -= 10;
 	$trivial->update_puntuacion($_SESSION['puntos'],$_SESSION['nombre']);
 	header("Location: ../old.preguntas_trivial.php");
--- htdocs/trivia/.test/old.preguntas_trivial.php
+++ htdocs/trivia/.test/old.preguntas_trivial.php
@@ -11,18 +11,17 @@ $respuestas = $trivial->get_respuestas($pregunta['id']);
 //las desordenamos
 shuffle($respuestas);
 ?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Juego de trivial</title>
+    <title>Trivia</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.css" -->
     <link type="text/css" rel="stylesheet" href="/css/bootstrap.css">
     <script src="/js/jquery.js"></script>
-    <!--script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>-->
 </head>
 
 <body>
@@ -39,27 +38,27 @@ shuffle($respuestas);
         <a href="#" class="text-left float-left pt-3"><img src="/images/back.png" width="50%"></a>
         <a href="#" class="text-right float-right pt-3"><img src="/images/gear.png" width="55%"></a>
         <div class="text-center row">
-            <!--Preguntas | Contenido Dinámico-->
             <h4 class="text-left col-lg-8">
                 <?=$pregunta['pregunta']?>
             </h4>
         </div>
     </div>
 
-    <div class="container row mx-auto">
+    <div class="container row mx-auto">        
         <div class="col-lg-8">
             <div class="align-elements-center">
-                <h3 class="float-left"><img class="align-self-center pr-3" src="/images/clock.png" style="height: 1.25em;"><span class="align-self-center font-weight-bold" id="time">20s</span></h3>
+                <h3 class="float-left"><img class="align-self-center pr-3" src="/images/clock.png" style="height: 1.25em;"><span class="align-self-center font-weight-bold countdown" id="time"></span></h3>
                 <h3 class="text-right"><span class="align-self-center font-weight-bold pr-3">
-                        <?php echo $_SESSION['puntos']?></span><img class="align-self-center" src="/images/point.png" style="height: 1.25em;"></h3>
+                        <?=$_SESSION['puntos']?></span><img class="align-self-center" src="/images/point.png" style="height: 1.25em;"></h3>
             </div>
-            <form method="post" id="form_responder" action="instancias/respuestas.php" accept-charset="utf-8" data-ajax="false">
+            <form method="post" id="form_responder" action="instancias/respuestas.php" class="pb-5">
+                <input type="hidden" name="correcta" value="<?=$pregunta['correcta']?>" />
                 <?php 
                     $i = 1;
                     foreach ($respuestas as $respuesta) {
                 ?>
                 <div class="row pt-2">
-                    <button type="submit" class="btn btn-info py-3 w-100">
+                    <button type="submit" class="btn btn-info py-3 w-100" value="<?=$respuesta['id']?>">
                         <p style="white-space: normal;" class="my-0">
                             <?=$respuesta['respuesta']?>
                             <?php $i++;?>
@@ -86,31 +85,28 @@ shuffle($respuestas);
             if (footerTop < docHeight) $('#footer').css('margin-top', 10 + (docHeight - footerTop) + 'px');
         });
 
-        function startTimer(duration, display) {
-            var timer = duration,
-                minutes, seconds;
-            setInterval(function() {
-                minutes = parseInt(timer / 60, 10)
-                seconds = parseInt(timer % 60, 10);
-
-                minutes = minutes < 10 ? "0" + minutes : minutes;
-                seconds = seconds < 10 ? "0" + seconds : seconds;
-
-                display.textContent = seconds + "s";
-
-                if (--timer < 0) {
-                    timer = 0;
+    </script>
+    <script>
+        $.fn.countdown = function(callback, duration, message) {
+            message = message || "";
+            var container = $(this[0]).html(duration + message);
+            var countdown = setInterval(function() {
+                if (--duration) {
+                    container.html(duration + message);
+                } else {
+                    clearInterval(countdown);
+                    callback.call(container);
                 }
-
             }, 1000);
-        }
-
-        window.onload = function() {
-            var time = 20,
-                display = document.querySelector('#time');
-            startTimer(time, display);
         };
-        
+
+        function redirect() {
+            window.location.href = "instancias/respuestas.php";
+            
+        }
+        $(document).ready(function() {
+            $(".countdown").countdown(redirect, 30, " s");
+        })
 
     </script>
 </body>
